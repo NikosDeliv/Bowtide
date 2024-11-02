4b41a17ecd7dce79455057f3a4103636a859066a
Nikos Delivos <delivosnikos@gmail.com>
2024-11-03 00:25:25 +0200

    Add files via upload

---
+++ Utilities/BowtideGlobalNPC.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using Bowtide.Content.Items.Accessories;
+
+namespace Bowtide.Utilities
+{
+    public class BowtideGlobalNPC : GlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+            if (shop.NpcType == NPCID.Dryad)
+            {
+                // This will make the Dryad sell the item in Overworld height (Forest) biome
+                shop.Add(ModContent.ItemType<ForestEmbrace>(), Condition.InOverworldHeight);
+            }
+        }
+    }
+}
